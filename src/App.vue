62e902afc334997302129c5edd49b6d711a297d4
satopocket <>
2019-08-09 16:32:56 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <form v-on:submit.prevent="addTask()">
+      <input type="text" v-model="newTask" />
+      <button>Button</button>
+    </form>
+    <hr />
+    <select v-model="showType">
+      <option value="all">タスクをすべて表示</option>
+      <option value="exceptDone">完了済みのタスクを表示しない</option>
+    </select>
+    <div class="task-list" v-for="task in taskList" :key="task.name">
+      <label v-if="isShow(task.done)" class="task-list__item">
+        <input type="checkbox" v-model="task.done" />
+        {{ task.name }}
+      </label>
     </div>
-    <router-view />
+    <p class="task-numbetr" v-cloak>
+      {{ getUncompletedNum() }}/{{ taskList.length }}
+    </p>
+    <pre>{{ $data }}</pre>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import Vue, { PropOptions } from "vue";
+interface Task {
+  name: string;
+  done: boolean;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+export default Vue.extend({
+  name: "app",
+  data() {
+    return {
+      taskList: [],
+      newTask: "",
+      showType: "all"
+    } as {
+      taskList: Task[];
+      newTask: string;
+      showType: string;
+    };
+  },
+  //   created() {
+  //       this.taskList.length = 0;
+  //   },
+  methods: {
+    addTask(): void {
+      let text = this.newTask.trim();
+      if (!text) {
+        return;
+      }
+      this.taskList.push({
+        name: text,
+        done: false
+      });
+      this.newTask = "";
+    },
+    isShow(done: boolean): boolean {
+      return (
+        this.showType === "all" ||
+        (this.showType === "exceptCompleted" && !done)
+      );
+    },
+    getUncompletedNum(): number {
+      let unCompletedTask = [];
+      for (const task of this.taskList) {
+        if (!task.done) {
+          unCompletedTask.push(task);
+        }
+      }
+      return unCompletedTask.length;
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@mixin flex-vender() {
+  display: flex;
+}
+.task-list {
+  @include flex-vender;
+  flex-direction: column;
+  align-items: center;
+  &__item {
+    width: 270px;
+    text-align: left;
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
     }
   }
 }
